ac86a1843bf980b6fd7a6e01502f41c86201e61c
Bhavi595 <yadavbhavirao@gmail.com>
2024-01-13 14:55:41 +0530

    version 1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+import Calculater from "./components/Calculater.js";
+
+
+const App = ()=>{
+
+   return <div className="main">
+    
+
+  <Calculater/>
+
+
+   </div>
+   
+
+  }
+
+  export default App;
